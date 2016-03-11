8a5c5b5a144ad5821ac01806751b3e5cb376a620
Shannon Hawn <shannonhawn@hotmail.com>
2016-03-11 17:53:40 -0500

    added headers

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var $body = $('body');
+var $headera = $('header');
+var $headerb = $('header');
+
+body.append$(header);
+headera.html('Below ground veggies');
+headerb.html('Above ground veggies');
